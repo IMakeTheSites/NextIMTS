9d4f7dd1527aa653ab2d2ad693ae7037cb448e65
Mark Wlodawski <imakethesites@gmail.com>
2024-12-16 18:56:21 -0500

    Install more packages to resemble previous

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,19 +1,47 @@
-import { Fragment } from "react";
+import Page from '../../components/Page';
+import Section from '../../components/Section/Section';
+import Cover from '../../components/Cover';
+import Title from '../../components/Title/Title';
+import PostGrid from '../../components/PostGrid/PostGrid';
+import Post from '../../components/Post/Post';
 
 function BlogPage() {
   return (
-    <Fragment>
-      <h1>The Blog Page</h1>
-      <ul>
-        <li>
-          <a href="https://imakethesites.com/headless-wordpress-is-it-for-you/">
-            Headless Wordpress
-          </a>
-        </li>
-        <li>Blank</li>
-      </ul>
-    </Fragment>
+    <Page title="Blog">
+      <div style={{ marginBottom: '1rem' }}>
+        <Section>
+          <Cover title="Blog" />
+        </Section>
+        <Section>
+          <Title>New Post</Title>
+          <PostGrid>
+            {posts.map((post) => (
+              <Post key={post._id} {...post} />
+            ))}
+          </PostGrid>
+          {/* {showLoadButton && (
+            <div className={{ display: 'flex', justifyContent: 'center' }}>
+              <Button disabled={loading} onClick={getMorePosts}>
+                Load More Posts
+              </Button>
+            </div>
+          )} */}
+        </Section>
+      </div>
+    </Page>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+  *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+  `);
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
 export default BlogPage;
